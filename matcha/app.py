af0cbcd6b89c23b1599053fd5fc705d393ab940f
kgomera <37669957+KudzanaiGomera@users.noreply.github.com>
2020-07-11 15:24:36 +0200

    root user update

--- matcha/app.py
+++ matcha/app.py
@@ -4,7 +4,7 @@ import bcrypt
 import re
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'matcha'
