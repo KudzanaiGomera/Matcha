d679b3b6aa788c8016d28f30bb5ac5f7bcefc3c8
KudzanaiGomera <kudzie@Lenovo2.localdomain>
2020-07-15 22:36:52 +0200

    upload and edit done

--- matcha/file.sql
+++ matcha/file.sql
@@ -9,28 +9,25 @@
         password VARCHAR(250) NOT NULL,
         email VARCHAR(250) NOT NULL UNIQUE,
         vkey VARCHAR(250) NOT NULL,
-        user_email_status enum('not verified','verified') NOT NULL,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        user_email_status enum('not verified','verified') NOT NULL
     );
 
     
   CREATE TABLE IF NOT EXISTS profiles(
         id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        user_id(11) NOT NULL UNIQUE,
+        user_id INT(11) NOT NULL UNIQUE,
         gender VARCHAR(250) NOT NULL UNIQUE,
         sexual_orientation VARCHAR(250) NOT NULL UNIQUE,
         bio VARCHAR(250) NOT NULL UNIQUE,
         listofinterest VARCHAR(250) NOT NULL UNIQUE,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
         FOREIGN KEY(user_id) REFERENCES accounts(id)
     );
 
 
    CREATE TABLE IF NOT EXISTS images(
         id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        image_id INT(11) NOT NULL,
-        imageFullName VARCHAR(500) NOT NULL,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY(image_id) REFERENCES accounts(id)
+        user_id INT(11) NOT NULL,
+        image_path VARCHAR(500) NOT NULL,
+        FOREIGN KEY(user_id) REFERENCES accounts(id)
     );
 
