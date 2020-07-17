9dd8d5c9385a8786c0409f1a4cb30c69553213b8
Joshua <joshua@Joshuas-MacBook-Pro.local>
2020-07-17 10:38:22 +0200

    quick push

--- matcha/app.py
+++ matcha/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL, MySQLdb
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 import bcrypt
 import re
